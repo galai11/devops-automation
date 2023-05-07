50549e06f521e834edc3839c899f8ba97ca3e831
unknown <galaijihed94@gmail.com~>
2023-05-07 17:18:27 +0200

    Here we are

--- src/main/java/com/galaijihed/devopsintegration/DevopsIntegrationApplication.java
+++ src/main/java/com/galaijihed/devopsintegration/DevopsIntegrationApplication.java
@@ -4,7 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+
 public class DevopsIntegrationApplication {
+	public void message(){
+		System.out.println("Here we are");
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DevopsIntegrationApplication.class, args);
